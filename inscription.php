bd931cbf8a43845090abf9d51c145e5184c95333
moahmed amine mezghich <git@mezghich.com>
2022-11-17 11:11:54 +0100

    solution exo 2

---
+++ inscription.php
@@ -0,0 +1,23 @@
+<?php
+$nom = $_POST["nom"];
+$prenom = $_POST["prenom"];
+$email = $_POST["email"];
+$date = date("Y-m-d H:i:s");
+
+
+$id = fopen("inscription.log", "a");
+if ($id==false) {
+    die("Erreur lors douverture de fichier");
+} else {
+    flock($id, LOCK_EX);
+    fwrite($id, "[$date] Une nouvelle inscription : $prenom $nom - $email"."\n");
+    flock($id, LOCK_UN);
+
+    $r=fclose($id);
+    if ($r==false) {
+        die("Erreur lors de la fermeture du fichier");
+    }
+}
+
+
+?>
